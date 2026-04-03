149e8b66235cbf5b10f5530b9da17edd87baa753
sinadavoudi286 <sina.davoudi.us@gmail.com>
2026-04-03 01:31:56 -0600

    yes

---
+++ Untitled.R
@@ -0,0 +1,4 @@
+data_dir <- "~/Library/Mobile Documents/com~apple~CloudDocs/My Research/Accounting Research/Political_Accounting/Political Misalignment/Codes/"
+
+# Example: loading data
+load(file.path(data_dir, "panel_full.rdata"))
